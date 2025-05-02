abb2794e5056fec8513f84d365e89b4b60a58aa1
Johnkenzzy <johnkenumeh@gmail.com>
2025-05-02 05:13:36 +0100

    Task3

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,13 @@
+-- List Glam rock bands ranked by their longevity (lifespan)
+SELECT
+    band_name,
+    (CASE 
+        WHEN split IS NULL OR split = 0 THEN 2022 - formed
+        ELSE split - formed
+     END) AS lifespan
+FROM
+    metal_bands
+WHERE
+    style = 'Glam rock'
+ORDER BY
+    lifespan DESC;
