39c06792e7c9cda409f9a5d90c0a758f7d7b2a6d
Johnkenzzy <johnkenumeh@gmail.com>
2025-05-02 05:58:58 +0100

    Fix

--- 0x00-MySQL_Advanced/8-index_my_names.sql
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -1,2 +1,6 @@
 -- Create the index on the first letter of the name column
+-- Drop the existing index if it already exists
+DROP INDEX IF EXISTS idx_name_first ON names;
+
+-- Create the index on the first letter of the 'name' column
 CREATE INDEX idx_name_first ON names (LEFT(name, 1));
