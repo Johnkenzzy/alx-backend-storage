056849b6d8426b29151fb8d8af0bcbcf9fca836c
Johnkenzzy <johnkenumeh@gmail.com>
2025-05-02 04:29:23 +0100

    Task1

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,7 @@
+-- Creates a table called user
+CREATE TABLE IF NOT EXISTS users (
+	id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+	email VARCHAR(225) NOT NULL UNIQUE,
+	name VARCHAR(255),
+	country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
+);
