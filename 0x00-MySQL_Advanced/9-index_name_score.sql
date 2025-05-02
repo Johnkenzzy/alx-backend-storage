f76b3caa022f85b249176bb2eb8453a7eb78fbf6
Johnkenzzy <johnkenumeh@gmail.com>
2025-05-02 05:59:58 +0100

    Fix

--- 0x00-MySQL_Advanced/9-index_name_score.sql
+++ 0x00-MySQL_Advanced/9-index_name_score.sql
@@ -1,2 +1,6 @@
 -- Create the index on the first letter of the name column and the score column
+-- Drop the existing index if it already exists
+DROP INDEX IF EXISTS idx_name_first_score ON names;
+
+-- Create the index on the first letter of 'name' and 'score'
 CREATE INDEX idx_name_first_score ON names (LEFT(name, 1), score);
